44b574165501dfa85c57a7c325ee63bbdb6a989e
GrandOichii <i.hawk@yandex.ru>
2021-12-05 18:33:37 +0400

    Now sorts with basic priority

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 import json
+from termcolor import colored
 
 TOKEN = open('TOKEN', 'r').read()
 banned_words = [word for word in open('banned_words.txt', 'r').read().split('\n')]
+priority_words = [word for word in open('priority_words.txt', 'r').read().split('\n')]
+blacklist_urls = [word for word in open('blacklist.txt', 'r').read().split('\n')]
 
 def vk_get_request(METHOD, PARAMS):
     url = 'https://api.vk.com/method/{}?{}&access_token={}&v={}'.format(METHOD, PARAMS, TOKEN, 5.131)
@@ -10,9 +13,19 @@ def vk_get_request(METHOD, PARAMS):
 
 def check_for_banned_words(item):
     text = item['text'].lower()
-    _=1
     for word in banned_words:
-        if word in text:
+        if word != '' and word in text:
+            return True
+    return False
+
+def check_for_photos(item):
+    result = 'attachments' in item
+    return result
+
+def check_priority(item):
+    text = item['text'].lower()
+    for word in priority_words:
+        if word != '' and word in text:
             return True
     return False
 
@@ -22,18 +35,32 @@ def filter(items):
     for item in items:
         if check_for_banned_words(item):
             continue
-        texts += [item['text']]
-        ids += ['{}_{}'.format(item['from_id'], item['id'])]
+        prioritized = check_priority(item) or check_for_photos(item)
+        text = item['text']
+        id = '{}_{}'.format(item['from_id'], item['id'])
+
+        if prioritized:
+            texts += [text]
+            ids += [id]
+        else:
+            texts.insert(0, text)
+            ids.insert(0, id)
     return [texts, ids]
 
-group_id = 'find_you_saratov'
+group_id = 'sosed499'
 count = 100
 vk_url = f'https://vk.com/{group_id}?w=wall{{}}'
 
 id = json.loads(vk_get_request('groups.getById', f'group_ids={group_id}'))['response'][0]['id']
 items = json.loads(vk_get_request('wall.get', f'owner_id=-{id}&count={count}'))['response']['items']
+items.reverse()
 result = filter(items)
 for i in range(len(result[0])):
-    print(result[0][i])
-    print(vk_url.format(result[1][i]))
+    url = vk_url.format(result[1][i])
+    if url in blacklist_urls:
+        continue
+    print(colored(result[0][i], 'white'))
+    print(colored(url, 'blue'))
+    if check_for_photos(items[i]):
+        print(colored('\tHAS PHOTO(ES)', 'red'))
     print('-' * 20)
